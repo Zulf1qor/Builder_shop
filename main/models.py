8bd7a5fb5f0f9e0d56633b4cc82ad42b1fef60e6
Zulf1qor <zsharibjonov@gmail.com>
2024-07-11 16:56:58 +0500

    Zulfiqot

--- main/models.py
+++ main/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='product_image/')
+    price = models.IntegerField()
+    discount = models.IntegerField()
+    status = models.BooleanField()
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='image/')
+    title = models.CharField(max_length=55)
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='blog_image/')
+    admin = models.CharField(max_length=25)
+    date = models.DateField(auto_now=True)
+
+
+class About(models.Model):
+    image = models.ImageField(upload_to='about_image/')
+    title = models.CharField(max_length=35)
+    describe = models.TextField()
+    best = models.CharField(max_length=55)
+    works = models.CharField(max_length=255)
+    expensice = models.CharField(max_length=255)
